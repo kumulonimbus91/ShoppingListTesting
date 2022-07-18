22a1083666202342fb6607199040d7b2651b202c
kumulonimbus91 <69019613+kumulonimbus91@users.noreply.github.com>
2022-07-18 21:14:47 +0200

    Initial commit

---
+++ app/src/main/java/com/nenad/shoppinglisttesting/data/local/ShoppingDao.kt
@@ -0,0 +1,24 @@
+package com.nenad.shoppinglisttesting.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertShoppingItem(shoppingItem: ShoppingItem)
+
+    @Delete
+    suspend fun deleteShoppingItem(shoppingItem: ShoppingItem)
+
+
+    @Query("SELECT * FROM shopping_items")
+    fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> //live data doesnt need suspend fun
+
+
+    @Query("SELECT SUM (price * amount) FROM shopping_items")
+    fun observeTotalPrice(): LiveData<Float>
+
+
+}
